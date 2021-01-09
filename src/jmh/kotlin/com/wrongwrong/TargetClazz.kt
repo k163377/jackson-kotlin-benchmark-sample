e18d5afd64ccb4625ed8d6d7b13b6f3a62894b7f
wrongwrong <boranti1995@gmail.com>
2021-01-09 14:35:18 +0900

    Fix benchmark to measure improvement

--- src/jmh/kotlin/com/wrongwrong/TargetClazz.kt
+++ src/jmh/kotlin/com/wrongwrong/TargetClazz.kt
@@ -1,6 +1,21 @@
 package com.wrongwrong
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
 annotation class NoArg
 
-@NoArg
-data class TargetClazz(val arg1: Int, val arg2: Int, val arg3: Int, val arg4: Int, val arg5: Int)
+// @NoArg
+data class TargetClazz(val foo: Int, val bar: Int, val baz: Int, val qux: Int, val quux: Int) {
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun creator(
+            @JsonProperty("foo") foo: Int,
+            @JsonProperty("bar") bar: Int,
+            @JsonProperty("baz") baz: Int,
+            @JsonProperty("qux") qux: Int,
+            @JsonProperty("quux") quux: Int
+        ) = TargetClazz(foo, bar, baz, qux, quux)
+    }
+}
