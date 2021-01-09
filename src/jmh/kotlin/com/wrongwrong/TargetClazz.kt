f3907566fc43dac1a87809e47ecf3890e4c77990
wrongwrong <boranti1995@gmail.com>
2021-01-09 12:34:20 +0900

    fix.

--- src/jmh/kotlin/com/wrongwrong/TargetClazz.kt
+++ src/jmh/kotlin/com/wrongwrong/TargetClazz.kt
@@ -3,10 +3,4 @@ package com.wrongwrong
 annotation class NoArg
 
 @NoArg
-data class TargetClazz(
-    val arg1: Int,
-    val arg2: Int,
-    val arg3: Int,
-    val arg4: Int,
-    val arg5: Int
-)
+data class TargetClazz(val arg1: Int, val arg2: Int, val arg3: Int, val arg4: Int, val arg5: Int)
