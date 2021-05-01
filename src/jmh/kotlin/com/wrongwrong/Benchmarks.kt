e04ced5ff04355105f152329fb7f290300e65d2c
wrongwrong <boranti1995@gmail.com>
2021-05-01 17:30:49 +0900

    update benchmark for feature

--- src/jmh/kotlin/com/wrongwrong/Benchmarks.kt
+++ src/jmh/kotlin/com/wrongwrong/Benchmarks.kt
@@ -1,7 +1,7 @@
 package com.wrongwrong
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
@@ -9,7 +9,7 @@ import org.openjdk.jmh.annotations.State
 @State(Scope.Benchmark)
 open class Benchmarks {
     private val javaMapper = ObjectMapper()
-    private val kotlinMapper = ObjectMapper().registerKotlinModule()
+    private val kotlinMapper = jacksonObjectMapper()
 
     private val expected = TargetClazz(1, 2, 3, 4, 5)
     private val input: ByteArray = javaMapper.writeValueAsBytes(expected)
