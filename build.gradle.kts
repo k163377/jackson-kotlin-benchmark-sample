2e25bf76851932bc293a7dbf0e3bba267fb45fd5
wrongwrong <boranti1995@gmail.com>
2022-01-10 20:24:47 +0900

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.5.31"
-    id("me.champeau.gradle.jmh") version "0.5.2"
+    id("me.champeau.gradle.jmh") version "0.5.3"
 }
 
 group = "com.wrongwrong"
@@ -14,10 +14,10 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.13.0")
+    implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.13.1")
 
     // use released
-    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.13.0")
+    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.13.1")
     // use local
     // implementation(files("\${Absolute path to jar}"))
     // implementation(kotlin("reflect"))
